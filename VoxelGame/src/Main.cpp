c8c9956226342cb08aae90f6026ba3791d99d71c
Bruno Vitte <bruno_vitte@hotmail.com>
2022-10-18 18:57:24 -0500

    add a simple exit

--- VoxelGame/src/Main.cpp
+++ VoxelGame/src/Main.cpp
@@ -2,6 +2,9 @@
 
 #include <GLFW/glfw3.h>
 
+void error_callback( int error, const char* description );
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+
 int main(void) {
     GLFWwindow* window;
 
@@ -10,7 +13,7 @@ int main(void) {
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(1080, 720, "Voxel Game", NULL, NULL);
     if (!window) {
         glfwTerminate();
         return -1;
@@ -18,6 +21,10 @@ int main(void) {
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+
+    glfwSetErrorCallback(error_callback);
+
+    glfwSetKeyCallback(window, key_callback);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window)) {
@@ -37,4 +44,13 @@ int main(void) {
     glfwTerminate();
 
     return 0;
+}
+
+void error_callback( int error, const char* description ){
+    std::cout << stderr << "Error: %s\n" << description << std::endl;
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods){
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
